d59600c42572b53e5e0b004b8e04acfb8bf8b5ce
Nanako Ohashi <nanako.ohashi@gmail.com>
2019-04-04 17:38:13 -0600

    refactor: import packages

---
+++ leading-causes-death.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import zipfile
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+get_ipython().run_line_magic('matplotlib', 'inline')
