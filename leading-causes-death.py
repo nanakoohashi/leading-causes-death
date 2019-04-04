e3be69e6b5e257f3cb17734a06754099c68d6e7b
Nanako Ohashi <nanako.ohashi@gmail.com>
2019-04-04 17:39:37 -0600

    refactor: read .csv file

--- leading-causes-death.py
+++ leading-causes-death.py
@@ -15,3 +15,8 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 # In[2]:
 with zipfile.ZipFile('leading-causes-of-death-and-numbers-of-deaths.zip', 'r') as myzip:
     myzip.extractall()
+
+    
+# In[3]:
+df_death = pd.read_csv('selected-trend-table-from-health-united-states-2011.-leading-causes-of-death-and-numbers-of-deaths-by-sex-race-and-hispanic-origin-united-states-1980-and-2009.csv')
+df_death
